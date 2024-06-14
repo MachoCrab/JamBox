a6b22c654970f3ec05e3d5a5a076911f2d067219
MachoCrab <137737859+MachoCrab@users.noreply.github.com>
2024-06-14 19:13:01 +1000

    hmmmm it seems I have forgotten the extent of how poorly written this code is. This commit is a shot in the dark, cannot test my code without it being run on the domain specified, and so localhost is no help. Not to worry, good thing I have lots of time.

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,16 @@
 var getDataButton = document.getElementById('get-data-button');
 
 if (getDataButton) {
-  getDataButton.addEventListener('click', function () {
-    document.getElementById('loading-screen').style.display = "flex";
-    authorizeSpotify();
-  });
+    getDataButton.addEventListener('click', function () {
+        document.getElementById('loading-screen').style.display = "flex";
+        authorizeSpotify();
+    });
 }
 
 function authorizeSpotify() {
     var clientId = '97a45e5baf1640f0bd179cc0137fe054';
     var redirectUri = 'https://emma-nipperess.github.io/JamBox/data.html';
 
-    // Set up the authorization URL
     var authParams = new URLSearchParams({
         client_id: clientId,
         response_type: 'token',
@@ -20,35 +19,23 @@ function authorizeSpotify() {
     });
     var authorizationUrl = 'https://accounts.spotify.com/authorize?' + authParams;
 
-    // Redirect the user to Spotify for authorization
     window.location.href = authorizationUrl;
 }
 
-// Extract access token from URL (Assuming it's passed as a URL parameter named 'access_token')
 function getAccessTokenFromURL() {
     const urlParams = new URLSearchParams(window.location.hash.substr(1));
     return urlParams.get('access_token');
 }
-  
-// After the user is redirected back from Spotify authorization, call fetchData
-document.addEventListener('DOMContentLoaded', function () {
-const accessToken = getAccessTokenFromURL();
 
-if (accessToken) {
-    //var loadingScreen = document.querySelector('.loading-screen');
-    //loadingScreen.classList.add('active');
-    fetchData(accessToken);
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const accessToken = getAccessTokenFromURL();
+    if (accessToken) {
+        fetchData(accessToken);
+    }
 });
 
-function getTerm() {
-    const term = document.getElementById("term").value;
-    console.log("Term" + term);
-    return term;
-}
-
 function getTopTracks(term) {
-    accessToken = getAccessTokenFromURL();
+    const accessToken = getAccessTokenFromURL();
     var headers = {
         'Authorization': 'Bearer ' + accessToken,
         'Content-Type': 'application/json'
@@ -62,8 +49,7 @@ function getTopTracks(term) {
                 artist: track.artists[0].name,
                 album: track.album.name,
                 image_url: track.album.images[0].url,
-                track_id: track.id,
-                access_token: accessToken
+                track_id: track.id
             }));
             return topTracks;
         })
@@ -74,7 +60,7 @@ function getTopTracks(term) {
 }
 
 function getRecentlyPlayed() {
-    accessToken = getAccessTokenFromURL();
+    const accessToken = getAccessTokenFromURL();
     var headers = {
         'Authorization': 'Bearer ' + accessToken,
         'Content-Type': 'application/json'
@@ -88,8 +74,7 @@ function getRecentlyPlayed() {
                 artist: track.track.artists[0].name,
                 album: track.track.album.name,
                 image_url: track.track.album.images[0].url,
-                track_id: track.track.id,
-                access_token: accessToken
+                track_id: track.track.id
             }));
             return recentTracks;
         })
@@ -99,64 +84,7 @@ function getRecentlyPlayed() {
         });
 }
 
-  
-// This function will be called after the user is redirected back from Spotify authorization
-function fetchDataOLD(accessToken) {
-    console.log("access token: " + accessToken);
-    // Use the access token to fetch data from Spotify API
-    var headers = {
-        'Authorization': 'Bearer ' + accessToken,
-        'Content-Type': 'application/json'
-    };
-
-
-    Promise.all([
-        fetch('https://api.spotify.com/v1/me/player/recently-played', { headers }),
-        fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50', { headers }),
-    ])
-    .then(responses => Promise.all(responses.map(response => response.json())))
-    .then(data => {
-        var recentTracks = data[0].items.map(track => ({
-            name: track.track.name,
-            artist: track.track.artists[0].name,
-            album: track.track.album.name,
-            image_url: track.track.album.images[0].url,
-            track_id: track.track.id,
-            access_token: accessToken
-        }));
-
-        var topTracks = data[1].items.map(track => ({
-            name: track.name,
-            artist: track.artists[0].name,
-            album: track.album.name,
-            image_url: track.album.images[0].url,
-            track_id: track.id,
-            access_token: accessToken
-        }));
-
-        getAllUserPlaylists(headers).then(playlists => {
-            console.log(playlists);
-            var allPlaylists = playlists.map(playlist => ({
-                url: playlist.external_urls['spotify'],
-                name: playlist.name
-            }));
-            renderData(allPlaylists, recentTracks, topTracks);
-        })   
-    })
-    .catch(error => console.error('Failed to fetch data:', error));
-
-    document.getElementById("loading-screen").display = "none";
-}
-
-function updateTopTracks(term) {
-    getTopTracks(term).then(data => {
-        
-        renderTopTracks(data);
-    });
-}
-
 function fetchData(accessToken) {
-    console.log("access token: " + accessToken);
     var headers = {
         'Authorization': 'Bearer ' + accessToken,
         'Content-Type': 'application/json'
@@ -164,42 +92,35 @@ function fetchData(accessToken) {
 
     Promise.all([
         getRecentlyPlayed(),
-        getTopTracks(getTerm())
+        getTopTracks('medium_term')
     ])
     .then(data => {
         var recentTracks = data[0];
         var topTracks = data[1];
 
         getAllUserPlaylists(headers).then(playlists => {
-            console.log(playlists);
             var allPlaylists = playlists.map(playlist => ({
                 url: playlist.external_urls['spotify'],
                 name: playlist.name
             }));
             renderData(allPlaylists, recentTracks, topTracks);
-        })   
+        });
     })
     .catch(error => console.error('Failed to fetch data:', error));
 
     document.getElementById("loading-screen").display = "none";
 }
 
-
 function renderTopTracks(topTracks) {
-    // Check if the ol element with id 'topTracksList' already exists
     var topTracksList = document.getElementById('topTracksList');
     if (!topTracksList) {
-        // If it doesn't exist, create a new ol element
         topTracksList = document.createElement('ol');
-        topTracksList.id = 'topTracksList'; // Set the id attribute
+        topTracksList.id = 'topTracksList';
         document.body.appendChild(topTracksList);
     } else {
-        // If it exists, clear its content to update with new data
         topTracksList.innerHTML = '';
     }
-    console.log(topTracks);
-    topTracks.forEach(function(track) {
-        // Create li elements for each track
+    topTracks.forEach(function (track) {
         var trackItem = document.createElement('li');
         var trackImage = document.createElement('img');
         trackImage.src = track.image_url;
@@ -208,52 +129,39 @@ function renderTopTracks(topTracks) {
         trackImage.height = '50';
         var trackInfo = document.createTextNode(`${track.name} - ${track.artist} (${track.album})`);
         var analyzeLink = document.createElement('a');
-        analyzeLink.href = `/analyze/${track.track_id}/${track.access_token}`;
+        analyzeLink.href = `/analyze/${track.track_id}`;
         analyzeLink.textContent = 'Analyze';
         trackItem.appendChild(trackImage);
         trackItem.appendChild(trackInfo);
         trackItem.appendChild(analyzeLink);
         topTracksList.appendChild(trackItem);
     });
-
-    console.log("Top tracks printed");
 }
 
-// inital load of the website
 function renderData(playlists, recentTracks, topTracks) {
-    console.log("attempting to render data");
-    console.log(topTracks)
-    console.log(playlists)
-    
-    // Render top tracks
     renderTopTracks(topTracks);
 
-    // Render playlists
-     var playlistsElement = document.createElement('h1');
-     playlistsElement.textContent = 'Your Playlists';
-     document.body.appendChild(playlistsElement);
- 
-     var playlistsList = document.createElement('ul');
-     playlists.forEach(function(playlist) {
-         var playlistItem = document.createElement('li');
-         var playlistLink = document.createElement('a');
-         playlistLink.href = playlist.url;
-         playlistLink.textContent = playlist.name;
-         playlistItem.appendChild(playlistLink);
-         playlistsList.appendChild(playlistItem);
-     });
-     document.body.appendChild(playlistsList);
+    var playlistsElement = document.createElement('h1');
+    playlistsElement.textContent = 'Your Playlists';
+    document.body.appendChild(playlistsElement);
 
+    var playlistsList = document.createElement('ul');
+    playlists.forEach(function (playlist) {
+        var playlistItem = document.createElement('li');
+        var playlistLink = document.createElement('a');
+        playlistLink.href = playlist.url;
+        playlistLink.textContent = playlist.name;
+        playlistItem.appendChild(playlistLink);
+        playlistsList.appendChild(playlistItem);
+    });
+    document.body.appendChild(playlistsList);
 
-     // Render top tracks 
-
-    // Render recent tracks
     var recentTracksElement = document.createElement('h1');
     recentTracksElement.textContent = 'Your Recent Tracks';
     document.body.appendChild(recentTracksElement);
 
     var recentTracksList = document.createElement('ul');
-    recentTracks.forEach(function(track) {
+    recentTracks.forEach(function (track) {
         var trackItem = document.createElement('li');
         var trackImage = document.createElement('img');
         trackImage.src = track.image_url;
@@ -262,23 +170,18 @@ function renderData(playlists, recentTracks, topTracks) {
         trackImage.height = '50';
         var trackInfo = document.createTextNode(`${track.name} - ${track.artist} (${track.album})`);
         var analyzeLink = document.createElement('a');
-        analyzeLink.href = `/analyze/${track.track_id}/${track.access_token}`;
+        analyzeLink.href = `/analyze/${track.track_id}`;
         analyzeLink.textContent = 'Analyze';
         trackItem.appendChild(trackImage);
         trackItem.appendChild(trackInfo);
         trackItem.appendChild(analyzeLink);
         recentTracksList.appendChild(trackItem);
     });
-    document.body.appendChild(recentTracksList);
-
-    // Remove the loading screen
-    //var loadingScreen = document.querySelector('.loading-screen');
-    //loadingScreen.classList.remove('active');
 }
 
 function getAllUserPlaylists(headers) {
     const playlistsUrl = 'https://api.spotify.com/v1/me/playlists';
-    const limit = 50; // Maximum number of playlists to retrieve per request
+    const limit = 50;
     let offset = 0;
     const allPlaylists = [];
 
@@ -300,13 +203,9 @@ function getAllUserPlaylists(headers) {
     function fetchAllPlaylists() {
         return fetchPlaylists().then(playlists => {
             if (playlists.length === 0) {
-                // No more playlists, return the accumulated results
                 return allPlaylists;
             } else {
-                // Add the fetched playlists to the accumulated results
                 allPlaylists.push(...playlists);
-
-                // Increment the offset and fetch the next set of playlists
                 offset += limit;
                 return fetchAllPlaylists();
             }
@@ -315,76 +214,7 @@ function getAllUserPlaylists(headers) {
 
     return fetchAllPlaylists();
 }
-//Function to update the playlist with new tracks
-function updatePlaylist() {
-    // Fetch the access token from the URL (same as before)
-    const accessToken = getAccessTokenFromURL();
 
-    // Use the access token to make requests to the Spotify API
-    var headers = {
-        'Authorization': 'Bearer ' + accessToken,
-        'Content-Type': 'application/json'
-    };
-
-    // Get all the user's playlists
-    getAllUserPlaylists(headers)
-        .then(playlists => {
-            // Find the playlist named "EVERYTHING DUMP !@#$%^&*("
-            const playlist = playlists.find(item => item.name === 'EVERYTHING DUMP !@#$%^&*(');
-
-            if (!playlist) {
-                // If the playlist doesn't exist, create it
-                createNewPlaylist(headers);
-            } 
-            // If the playlist exists, get all its tracks and the tracks from other playlists
-            const playlistId = playlist.id;
-            getAllTracksFromPlaylist(playlistId, headers)
-                .then(dumpTracks => {
-                    getAllOtherTracks(playlists, playlistId, headers)
-                        .then(otherTracks => {
-                            const uniqueTracks = getUniqueTracks(otherTracks, dumpTracks);
-                            if (uniqueTracks.length > 0) {
-                                console.log("adding songs to playlist")
-                                // Add the unique tracks to the playlist
-                                addAllTracksToPlaylist(uniqueTracks, playlistId, headers);
-                            }
-                            // Redirect back to the main page after updating the playlist
-                            //window.location.href = '/';
-                        })
-                        .catch(error => console.error('Failed to fetch other tracks:', error));
-                })
-                .catch(error => console.error('Failed to fetch dump tracks:', error));
-        })
-        .catch(error => console.error('Failed to fetch playlists:', error));
-}
-
-// Function to create a new playlist named "EVERYTHING DUMP !@#$%^&*("
-function createNewPlaylist(headers) {
-    const createPlaylistUrl = 'https://api.spotify.com/v1/me/playlists';
-    const request_data = {
-        name: 'EVERYTHING DUMP !@#$%^&*(',
-        public: false,
-        description: 'A playlist containing every song from all other playlists'
-    };
-    fetch(createPlaylistUrl, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(request_data)
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.id) {
-                console.log('Playlist created successfully:', data.name);
-            } else {
-                console.error('Failed to create playlist');
-            }
-            // Redirect back to the main page after creating the playlist
-            window.location.href = '/';
-        })
-        .catch(error => console.error('Failed to create playlist:', error));
-}
-
-// Function to get all tracks from a playlist
 async function getAllTracksFromPlaylist(playlistId, headers) {
     const limit = 50;
     let offset = 0;
@@ -414,12 +244,9 @@ async function getAllTracksFromPlaylist(playlistId, headers) {
             break;
         }
     }
-    console.log(allTracks);
     return allTracks;
 }
 
-
-// Function to get all tracks from other playlists except the "EVERYTHING DUMP !@#$%^&*(" playlist
 function getAllOtherTracks(playlists, dumpPlaylistId, headers) {
     const allOtherTracks = [];
     const promises = [];
@@ -442,31 +269,22 @@ function getAllOtherTracks(playlists, dumpPlaylistId, headers) {
         });
 }
 
-// Function to get unique tracks by comparing dump tracks with other tracks
 function getUniqueTracks(otherTracks, dumpTracks) {
     const uniqueTracks = [];
     const dumpTrackURIs = dumpTracks.map(track => track.track.uri);
-    var errorCount = 0;
-    console.log("attempting to find unique tracks");
     for (const track of otherTracks) {
-        console.log(track.track);
         try {
             const trackURI = track.track.uri;
             if (!dumpTrackURIs.includes(trackURI)) {
                 uniqueTracks.push(trackURI);
             }
-        } 
-        catch(error) {
-            errorCount = errorCount + 1;
-            console.log("error number: " + errorCount + ". " + error.message);
+        } catch (error) {
+            console.log("Error: " + error.message);
         }
-        
     }
-    console.log(" unique tracks: " + (uniqueTracks));
     return uniqueTracks;
 }
 
-// Function to add all tracks to the playlist
 function addAllTracksToPlaylist(trackURIs, playlistId, headers) {
     const addTracksUrl = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
     const batchSize = 100;
@@ -490,4 +308,67 @@ function addAllTracksToPlaylist(trackURIs, playlistId, headers) {
     }
 }
 
+document.getElementById('update-playlist-button').addEventListener('click', () => {
+    const playlistName = document.getElementById('playlist-name').value;
+    if (playlistName) {
+        updatePlaylist(playlistName);
+    } else {
+        alert('Please enter a playlist name.');
+    }
+});
 
+function updatePlaylist(playlistName) {
+    const accessToken = getAccessTokenFromURL();
+
+    var headers = {
+        'Authorization': 'Bearer ' + accessToken,
+        'Content-Type': 'application/json'
+    };
+
+    getAllUserPlaylists(headers)
+        .then(playlists => {
+            const playlist = playlists.find(item => item.name === playlistName);
+
+            if (!playlist) {
+                createNewPlaylist(headers, playlistName);
+            } else {
+                const playlistId = playlist.id;
+                getAllTracksFromPlaylist(playlistId, headers)
+                    .then(dumpTracks => {
+                        getAllOtherTracks(playlists, playlistId, headers)
+                            .then(otherTracks => {
+                                const uniqueTracks = getUniqueTracks(otherTracks, dumpTracks);
+                                if (uniqueTracks.length > 0) {
+                                    addAllTracksToPlaylist(uniqueTracks, playlistId, headers);
+                                }
+                            })
+                            .catch(error => console.error('Failed to fetch other tracks:', error));
+                    })
+                    .catch(error => console.error('Failed to fetch dump tracks:', error));
+            }
+        })
+        .catch(error => console.error('Failed to fetch playlists:', error));
+}
+
+function createNewPlaylist(headers, playlistName) {
+    const createPlaylistUrl = 'https://api.spotify.com/v1/me/playlists';
+    const request_data = {
+        name: playlistName,
+        public: false,
+        description: 'A playlist containing every song from all other playlists'
+    };
+    fetch(createPlaylistUrl, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(request_data)
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.id) {
+                console.log('Playlist created successfully:', data.name);
+            } else {
+                console.error('Failed to create playlist');
+            }
+        })
+        .catch(error => console.error('Failed to create playlist:', error));
+}
